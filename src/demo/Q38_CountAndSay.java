4fac6938a76530e85e5c31f05e01a9ffb6df225f
Yan19910828 <35778262+Yan19910828@users.noreply.github.com>
2019-11-06 23:43:25 -0500

    Q38 CountAndSay by using recursive method

--- src/demo/Q38_CountAndSay.java
+++ src/demo/Q38_CountAndSay.java
@@ -3,17 +3,37 @@ package demo;
 public class Q38_CountAndSay {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(countAndSay(1));
+		System.out.println(countAndSay(2));
+		System.out.println(countAndSay(3));
+		System.out.println(countAndSay(4));
+		System.out.println(countAndSay(5));
 	}
 
 	public static String countAndSay(int n) {
 		if (n == 1)
 			return "1";
-		StringBuilder res = new StringBuilder();
-		String str = countAndSay(n - 1);
 
-		return null;
+		StringBuilder sb = new StringBuilder();
+		String res = countAndSay(n - 1);
+		char current = res.charAt(0);
+		int count = 0;
+		for (int i = 0; i < res.length(); i++) {
+
+			if (current == res.charAt(i)) {
+				count++;
+			} else {
+				sb.append(count);
+				sb.append(current);
+				current = res.charAt(i);
+				count = 1;
+			}
+		}
+		sb.append(count);
+		sb.append(current);
+
+//		System.out.println(sb.toString());
+		return sb.toString();
 
 	}
 
